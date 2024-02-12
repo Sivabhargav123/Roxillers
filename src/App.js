16e5da8f2a4c85f68d5579330789fde8d21f9a0b
Sivabhargav123 <gsiva52892@gmail.com>
2024-02-12 19:36:05 +0000

    roxillers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+//import axios from "axios";
 
-function App() {
+const TransactionList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to ensure the effect runs only once
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Products</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <p>Description: {product.description}</p>
+            <p>Category: {product.category}</p>
+            <img
+              src={product.image}
+              alt={product.title}
+              style={{ maxWidth: "200px" }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TransactionList;
